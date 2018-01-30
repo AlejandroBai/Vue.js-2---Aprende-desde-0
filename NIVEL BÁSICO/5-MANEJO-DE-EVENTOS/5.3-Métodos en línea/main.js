a0eb16e361ac9aef53b60bbaa368d9121821042b
Alejandro Bayo Saiz <alexbayosaiz@outlook.es>
2018-01-30 18:33:36 +0100

    borrado rm -r

--- NIVEL BÁSICO/5-MANEJO-DE-EVENTOS/5.3-Métodos en línea/main.js
+++
@@ -1,8 +0,0 @@
-new Vue({
-  el: '#app',
-  methods: {
-    saludar: function(mensaje) {
-      return alert('Vue.js dice: ' + mensaje);
-    }
-  }
-});
--- NIVEL BÁSICO/5-MANEJO-DE-EVENTOS/5.4-Parámetro especial $event/main.js
+++
@@ -1,9 +0,0 @@
-new Vue({
-  el: '#app',
-  methods: {
-    atencion: function(mensaje, event) {
-      if (event) event.preventDefault()
-      alert(mensaje)
-    }
-  }
-});
