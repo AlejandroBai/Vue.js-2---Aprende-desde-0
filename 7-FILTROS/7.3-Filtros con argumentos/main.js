3fa7b206ef5bc2eb69d3a7a2ac3e9fc91e77578d
Alejandro Bayo Saiz <alexbayosaiz@outlook.es>
2018-01-30 18:01:26 +0100

    Nivel basico terminado

--- 7-FILTROS/7.3-Filtros con argumentos/main.js
+++ 7-FILTROS/7.3-Filtros con argumentos/main.js
@@ -1,30 +1,12 @@
 new Vue({
   el: '#app',
   data: {
-    citas: [
-      {
-        autor: 'Homer Simpson',
-        cita: 'Tendrá todo el dinero del mundo, pero hay una cosa que nunca podrá comprar... ¡Un dinosaurio!'
-      },
-      {
-        autor: 'Homer Simpson',
-        cita: 'Lisa, los vampiros son seres inventados, como los duendes, los gremlins y los esquimales'
-      },
-      {
-        autor: 'Ralph Wiggum',
-        cita: '¡Corre, plátano!'
-      },
-      {
-        autor: 'Barney Gumble',
-        cita: 'No hay nada mejor que la cerveza para darle a uno esa falsa sensación de bienestar'
-      },
-    ]
+    mensaje: 'Hola',
+    pregunta: '¿Cómo estás?'
   },
-  methods: {
-    citasAutor: function(autor){
-      return this.citas.filter(function(cita){
-        return cita.autor === autor
-      });
+  filters: {
+    saludo(valor, nombre, pregunta) {
+      return valor + ' ' + nombre + ' ' + pregunta;
     }
   }
 });
